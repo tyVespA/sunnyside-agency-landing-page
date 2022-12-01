7655813a1089d317ea9473911c2704597dfe3fee
tyVespA <migliavacca.marco08@gmail.com>
2022-12-01 13:02:13 +0100

    Add mobile menu script and animation
    TODO:
    - script for buttons with underline
    - hover effects

--- script.js
+++ script.js
@@ -1,14 +1,27 @@
-const underlinedBtns = document.querySelectorAll(".underlined-btn")
-const underlines = document.querySelectorAll(".underline")
+// const underlinedBtns = document.querySelectorAll(".underlined-btn")
+// const underlines = document.querySelectorAll(".underline")
 
-underlinedBtns.forEach((underlinedBtn) => {
-   underlinedBtn.addEventListener("mouseover", () => {
-      underlines.classList.add("underline-mouseover")
-   })
-})
+// underlinedBtns.forEach((underlinedBtn) => {
+//    underlinedBtn.addEventListener("mouseover", () => {
+//       underlines.classList.add("underline-mouseover")
+//    })
+// })
 
 // underlinedBtns.forEach((underlinedBtn) => {
 //    underlinedBtn.addEventListener("mouseout", () => {
 //       underlines.classList.remove("underline-mouseover")
 //    })
 // })
+
+// HAMBURGER MENU POPUP
+
+const hamburger = document.querySelector(".hamburger")
+const mobileMenu = document.querySelector(".mobile-menu")
+const arrow = document.querySelector(".arrow")
+
+hamburger.addEventListener("click", () => {
+   hamburger.classList.toggle("lower-opacity")
+   // mobileMenu.classList.toggle("hidden")
+   mobileMenu.classList.toggle("slide-left")
+   arrow.classList.toggle("fade-away")
+})
